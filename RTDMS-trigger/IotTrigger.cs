dbc601f313ee8c937002bc59a3cf9a69382f8067
Alec Barran <alexanderbarran@gmail.com>
2023-07-19 09:09:23 -0400

    static vars for Azure durability

--- RTDMS-trigger/IotTrigger.cs
+++ RTDMS-trigger/IotTrigger.cs
@@ -15,19 +15,22 @@ namespace viceroy
 {
     public class IotTrigger
     {
-        int temperatureThreshold = 76;
+        static int temperatureThreshold = 76;
         private static HttpClient client = new HttpClient();
-        int minimumEmailInterval = 3600000; // minimum milliseconds between emails, no more than 30/day for IFTTT free tier
-        DateTime nextEmailTime = new DateTime();
+        static int minimumEmailInterval = 3600000; // minimum milliseconds between emails, no more than 30/day for IFTTT free tier
+        static DateTime nextEmailTime = new DateTime();
         public record ShortTelem(string deviceId, double temperature, double pressure); // IFTTT limits JSON payloads to 3 items
-        string iftttUrl = System.Environment.GetEnvironmentVariable("IftttUrl", EnvironmentVariableTarget.Process);
+        static string iftttUrl = System.Environment.GetEnvironmentVariable("IftttUrl", EnvironmentVariableTarget.Process);
 
         [FunctionName("IotTrigger")]
-        public async Task Run([IotHubTrigger("messages/events", Connection = "ConnectionString")] EventData message, ILogger log)
+        public static async Task Run([IotHubTrigger("messages/events", Connection = "ConnectionString")] EventData message, ILogger log)
         {
-            // capture the telemetry in an instance of the telemetry model variable
             var jsonPayload = Encoding.UTF8.GetString(message.Body.ToArray());
+            // capture the telemetry in an instance of the telemetry model variable
+            // nullable to suppress warning CS8632
+            #nullable enable
             Telem? telemetryDataPoint = JsonSerializer.Deserialize<Telem>(jsonPayload);
+            #nullable disable
 
             // check whether current temp is above threshold and past minimum email time
             if (telemetryDataPoint.temperature > temperatureThreshold && DateTime.Now > nextEmailTime)
@@ -36,7 +39,7 @@ namespace viceroy
             }
         }
 
-                public async Task SendEmailNotification(Telem abnormalDataPoint, ILogger log)
+        public static async Task SendEmailNotification(Telem abnormalDataPoint, ILogger log)
         {
             // send POST request with telemetry as payload
             // https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/http/httpclient
@@ -52,17 +55,17 @@ namespace viceroy
             // check response code of IFTTT call
             if (response.IsSuccessStatusCode)
             {
-                log.LogInformation("Successfully sent webhook to IFTTT.");
-                // do not send another email for at least an hour
-                nextEmailTime = DateTime.Now;
-                nextEmailTime.AddMilliseconds(minimumEmailInterval);
+                // do not send another email for at least minimumEmailInterval milliseconds (3600000ms == 1hr)
+                nextEmailTime = DateTime.Now.AddMilliseconds(minimumEmailInterval);
+                var nextEmailString = nextEmailTime.ToString(System.Globalization.CultureInfo.InvariantCulture);
+                log.LogInformation($"Successfully sent webhook to IFTTT. Next email at {nextEmailString}.");
             }
             else
             {
-                log.LogWarning("IFTTT call failed. Retrying in 1 minute...");
                 // do not attempt another IFTTT call for one minute
-                nextEmailTime = DateTime.Now;
-                nextEmailTime.AddMinutes(1);
+                nextEmailTime = DateTime.Now.AddMinutes(1);
+                var nextEmailString = nextEmailTime.ToString(System.Globalization.CultureInfo.InvariantCulture);
+                log.LogError($"IFTTT webhook failed. Retrying at {nextEmailString}.");
             }
         }
     }
